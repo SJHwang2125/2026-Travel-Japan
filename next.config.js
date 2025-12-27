85f0ccfd5d638508a6874d6e3911a607a83e343d
황성진 <hwangseongjin@hwangseongjins-Mac-mini.local>
2025-12-27 19:33:21 +0900

    Configure gh pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 정적 내보내기 설정
+  images: {
+    unoptimized: true, // GitHub Pages는 이미지 최적화 기능을 지원하지 않음
+  },
+  // 저장소 이름에 맞게 경로 설정 (중요!)
+  basePath: '/2026-Travel-Japan',
+  assetPrefix: '/2026-Travel-Japan',
+};
 
 module.exports = nextConfig;
